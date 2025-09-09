9f0d8cd4f3ad9819da4a87a2ce436d140a21a549
NaderMohamed325 <nadoorclasher@gmail.com>
2025-09-09 14:29:33 +0300

    feat: add getter and setter methods to School entity

--- src/main/java/com/nader/studentmanager/entity/School.java
+++ src/main/java/com/nader/studentmanager/entity/School.java
@@ -18,5 +18,27 @@ public class School {
     @OneToMany(mappedBy = "school", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     List<Student> students;
 
+    public String getLocation() {
+        return location;
+    }
 
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
 }
