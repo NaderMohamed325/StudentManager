ccd97520ec15885ad8b47f0cf0fabd9f1315624f
NaderMohamed325 <nadoorclasher@gmail.com>
2025-09-09 14:49:26 +0300

    feat: integrate Lombok annotations into School entity for cleaner code

--- src/main/java/com/nader/studentmanager/entity/School.java
+++ src/main/java/com/nader/studentmanager/entity/School.java
@@ -1,44 +1,36 @@
 package com.nader.studentmanager.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "schools")
 public class School {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long id;
 
     @Column
+    @Getter
+    @Setter
+    private String name;
+
+
+    @Column
+    @Getter
+    @Setter
     private String location;
 
 
+    @Getter
+    @Setter
     @OneToMany(mappedBy = "school", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     List<Student> students;
 
-    public String getLocation() {
-        return location;
-    }
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
 }
