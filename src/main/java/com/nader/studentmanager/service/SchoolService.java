de57e394ddaa0a3460507798c56253202ef93232
NaderMohamed325 <nadoorclasher@gmail.com>
2025-09-09 15:01:16 +0300

    feat: extend SchoolService with delete, update, and list methods for managing schools

--- src/main/java/com/nader/studentmanager/service/SchoolService.java
+++ src/main/java/com/nader/studentmanager/service/SchoolService.java
@@ -5,6 +5,21 @@ import com.nader.studentmanager.repository.SchoolRepository;
 import com.nader.studentmanager.service.dto.AddSchoolDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+/**
+ * Service class for managing School entities.
+ * Provides methods to add, retrieve, update, and delete schools.
+ * Uses SchoolRepository for database operations.
+ * Methods:
+ * - addSchool(AddSchoolDto school): Adds a new school.
+ * - getSchoolById(Long id): Retrieves a school by its ID.
+ * - deleteSchool(Long id): Deletes a school by its ID.
+ * - updateSchool(Long id, AddSchoolDto schoolDto): Updates an existing school's details.
+ * - getAllSchools(): Retrieves a list of all schools.
+ *
+ * @Author Nader
+ */
 @Service
 public class SchoolService {
     private final SchoolRepository schoolRepository;
@@ -23,5 +38,23 @@ public class SchoolService {
     public School getSchoolById(Long id) {
         return schoolRepository.findById(id).orElse(null);
     }
+
+    public void deleteSchool(Long id) {
+        schoolRepository.deleteById(id);
+    }
+
+    public School updateSchool(Long id, AddSchoolDto schoolDto) {
+        return schoolRepository.findById(id).map(school -> {
+            school.setName(schoolDto.getName());
+            school.setLocation(schoolDto.getLocation());
+            return schoolRepository.save(school);
+        }).orElse(null);
+    }
+
+    public List<School> getAllSchools() {
+        return schoolRepository.findAll();
+    }
+
+
 
 }
