4c3550a747b84e864f5524dbdea89a107cc9bae4
NaderMohamed325 <nadoorclasher@gmail.com>
2025-09-09 14:29:42 +0300

    feat: add getter and setter methods to Student entity

--- src/main/java/com/nader/studentmanager/entity/Student.java
+++ src/main/java/com/nader/studentmanager/entity/Student.java
@@ -24,4 +24,43 @@ public class Student {
     @JoinColumn(name = "school_id")
     private School school;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
 }
